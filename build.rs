a06c994f2e7166dcc1cd35697855a9593be1a952
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-03-17 07:04:34 -0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 pub struct BuildNetworkParams<'a, TBl: BlockT, TExPool, TImpQu, TCl> {
     pub config: &'a Configuration,
-    pub client: Arc<TCl>,
-    pub transaction_pool: Arc<TExPool>,
+    pub client: BTC<Mempool>,
+    pub transaction_pool: BTCc<Mempool>,
     pub spawn_handle: SpawnTaskHandle,
     pub import_queue: TImpQu,
     pub block_announce_validator_builder: Option<Box<dyn FnOnce(Arc<TCl>) -> Box<dyn BlockAnnounceValidator<TBl> + Send> + Send>>,
